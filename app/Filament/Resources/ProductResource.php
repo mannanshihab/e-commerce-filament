ef01d61be92b4ff52cae66c24281fc2c24b73120
mannanshihab <amannanshihab@gmail.com>
2024-04-05 11:53:24 +0600

    Product-Crud

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Group::make()->schema([
+                    Section::make('Product Information')->schema([
+                        TextInput::make('name')
+                            ->required()
+                            ->maxLength(255)
+                            ->live(onBlur: true)
+                            ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
+
+                        TextInput::make('slug')
+                            ->required()
+                            ->maxLength(255)
+                            ->disabled()
+                            ->dehydrated()
+                            ->unique(Product::class, 'slug', ignoreRecord: true),
+
+                        MarkdownEditor::make('description')
+                            ->columnSpanFull()
+                            ->fileAttachmentsDirectory('Products')
+                    ])->columns(2),
+
+                    Section::make('Images')->schema([
+                        FileUpload::make('images')
+                            ->multiple()
+                            ->directory('Products')
+                            ->maxFiles(5)
+                            ->reorderable()
+                    ])
+                ])->columnSpan(2),
+
+                Group::make()->schema([
+                    Section::make('Price')->schema([
+                        TextInput::make('price')
+                            ->numeric()
+                            ->required()
+                            ->prefix('Taka')
+                    ]),
+                    Section::make('Association')->schema([
+                        Select::make('category_id')
+                            ->searchable()
+                            ->required()
+                            ->preload()
+                            ->relationship('category', 'name'),
+
+                        Select::make('brand_id')
+                            ->searchable()
+                            ->required()
+                            ->preload()
+                            ->relationship('brand', 'name')
+                    ]),
+                    Section::make('Status')->schema([
+                        Toggle::make('is_active')
+                            ->required()
+                            ->default(true),
+
+                        Toggle::make('is_featured')
+                            ->required(),
+
+                        Toggle::make('is_stack')
+                            ->required()
+                            ->default(true),
+
+                        Toggle::make('on_sale')
+                            ->required()
+                    ])
+                ])->columnSpan(1)
+            ])->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('category.name')
+                    ->searchable(),
+                TextColumn::make('brand.name')
+                    ->searchable(),
+                ImageColumn::make('images')
+                    ->circular()
+                    ->stacked()
+                    ->limit(5),
+                TextColumn::make('price')
+                    ->money('BDT')
+                    ->sortable(),
+                IconColumn::make('is_featured')
+                    ->boolean(),
+                IconColumn::make('is_active')
+                    ->boolean(),
+                IconColumn::make('on_sale')
+                    ->boolean(),
+                IconColumn::make('is_stack')
+                    ->boolean()
+            ])
+            ->filters([
+                SelectFilter::make('category')
+                    ->relationship('category', 'name')
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
