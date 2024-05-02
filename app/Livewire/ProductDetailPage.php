d7e60657bf1fd9b21f29c0da90f1a0bda3d0d088
mannanshihab <amannanshihab@gmail.com>
2024-05-02 12:30:02 +0600

    Product-Page-Dynamic

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class ProductDetailPage extends Component
 {
+    #[Title('Product Details - SliceTech')]
+
+    public $slug;
+
+    public function mount($slug){
+        $this->slug = $slug;
+    }
+
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page',[
+            'product' => Product::where('slug', $this->slug)->firstOrFail(),
+        ]);
     }
 }
