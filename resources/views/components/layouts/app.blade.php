5539d5c8c20f7e2a4454f9faea9adf62e49da586
mannanshihab <amannanshihab@gmail.com>
2024-05-04 12:51:04 +0600

    update-meta

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,8 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <meta name="description" 
+              content="Discover a world of shopping at our E-commerce platform! 
+              From trendy fashion to cutting-edge gadgets, we’ve got it all. 
+              Browse, click, and shop with confidence. 
+              Your next favorite item is just a click away !">
+        <meta name="author" content="Mannan Shihab">
         <title>{{ $title ?? 'My Shop' }}</title>
+        <link rel="icon" href="{{ asset('icon/SliceTech.png') }}">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
