da6858e0438a4b2a00e6911d6b4f6dc0f1722a3c
mannanshihab <amannanshihab@gmail.com>
2024-05-15 13:32:27 +0600

    add-to-cart

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Product;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -11,11 +13,25 @@ class ProductDetailPage extends Component
     #[Title('Product Details - SliceTech')]
 
     public $slug;
+    public $quantity =  1;
 
     public function mount($slug){
         $this->slug = $slug;
     }
 
+    public function increseQty(){
+        $this->quantity++;
+    }
+    public function decreseQty(){
+        if($this->quantity > 1){
+            $this->quantity--;
+        }
+    }
+    #add to cart
+    public function addToCart($product_id){
+        $total_count = CartManagement::addItemToCart($product_id);
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+    }
     public function render()
     {
         return view('livewire.product-detail-page',[
--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
@@ -33,6 +35,12 @@ class ProductsPage extends Component
 
     #[Url]
     public $sort;
+
+    #add to cart
+    public function addToCart($product_id){
+        $total_count = CartManagement::addItemToCart($product_id);
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+    }
 
     public function render()
     {
